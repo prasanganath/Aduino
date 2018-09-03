d0046919160e82c88dec8ea15092ea9004469f1e
sandun prasanganath <33305140+prasanganath@users.noreply.github.com>
2018-09-03 21:53:37 +0530

    Add files via upload

---
+++ sketch_sep16a/sketch_sep16a.ino
@@ -0,0 +1,31 @@
+void setup() {
+  pinMode(1,OUTPUT);
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+ 
+}
+void loop() {
+  int i=1;
+  int q=4;
+ for(i=1; i<5; i++){
+  digitalWrite(i,HIGH);
+  delay(1000);
+  digitalWrite(i,LOW);
+  delay(10);
+   digitalWrite(i+1,HIGH);
+  delay(1000);
+  digitalWrite(i+1,LOW);
+  delay(10);
+  
+  
+ }
+    for(q=4; q>0; q--){
+    digitalWrite(q,HIGH);
+    delay(50);
+    digitalWrite(q,LOW);
+    delay(10);
+    }
+  }
+ 
+
