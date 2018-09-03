4431e630c3cce94d44a7d8644a2b92800dc19f1e
sandun prasanganath <33305140+prasanganath@users.noreply.github.com>
2018-09-03 21:51:15 +0530

    Add files via upload

---
+++ sketch_aug24a/sketch_aug24a.ino
@@ -0,0 +1,38 @@
+int L = 12;
+int E = 13;
+int f =11;
+int g =10;
+int h=9;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(L,OUTPUT);
+  pinMode(E,OUTPUT);
+  pinMode(f,OUTPUT);
+  pinMode(g,OUTPUT);
+  pinMode(h,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(L,HIGH);
+  delay(100);
+  digitalWrite(L,LOW);
+  delay(30);
+  digitalWrite(E,HIGH);
+  delay(100);
+  digitalWrite(E,LOW);
+  delay(30);
+  digitalWrite(f,HIGH);
+  delay(100);
+  digitalWrite(f,LOW);
+  delay(30);
+  digitalWrite(g,HIGH);
+  delay(100);
+  digitalWrite(g,LOW);
+  delay(50);
+  digitalWrite(h,HIGH);
+  delay(100);
+  digitalWrite(h,LOW);
+  delay(30);
+  
+}
