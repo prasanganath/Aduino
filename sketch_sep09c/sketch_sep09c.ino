f0254a31d76469a469298c22ee0fbcf0d5dd3dc2
sandun prasanganath <33305140+prasanganath@users.noreply.github.com>
2018-09-03 21:53:17 +0530

    Add files via upload

---
+++ sketch_sep09c/sketch_sep09c.ino
@@ -0,0 +1,26 @@
+char val;
+void setup() {
+  pinMode(13,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available()){
+    val=Serial.read();
+    Serial.println(val);
+  }
+
+  if (val=='1'){
+    digitalWrite(13,HIGH);
+  }else if(val=='2'){
+    digitalWrite(13,LOW);
+  }else if (val=='3'){
+    while (true){
+      digitalWrite(13,HIGH);
+      delay(100);
+      digitalWrite(13,LOW);
+      delay(100);
+    }
+  }
+}
