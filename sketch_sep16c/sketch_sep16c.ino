2031f1ecc50419a416dbf25241d19e56f31bca6c
sandun prasanganath <33305140+prasanganath@users.noreply.github.com>
2018-09-03 21:54:32 +0530

    Add files via upload

---
+++ sketch_sep16c/sketch_sep16c.ino
@@ -0,0 +1,21 @@
+pinMode(1,OUTPUT);
+pinMode(2,OUTPUT);
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+digitalWrite(1,HIGH);
+delay(1000);
+digitalWrite(2,HIGH);
+delay(1000);
+digitalWrite(1,LOW);
+digitalWrite(2,LOW)
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
