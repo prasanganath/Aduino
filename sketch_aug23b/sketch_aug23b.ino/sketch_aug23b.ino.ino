d20a69d7b535ec86201c7456a7f39dc6bc7b607f
sandun prasanganath <33305140+prasanganath@users.noreply.github.com>
2018-09-03 21:50:32 +0530

    Add files via upload

---
+++ sketch_aug23b/sketch_aug23b.ino/sketch_aug23b.ino.ino
@@ -0,0 +1,16 @@
+byte LED =13;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode (LED,OUTPUT);
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED,HIGH);
+  delay(10000);
+  digitalWrite(LED,LOW);
+  delay(1000);
+
+}
