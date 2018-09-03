7bc700bd8ffa25010c2116bc7a667881d49719b6
sandun prasanganath <33305140+prasanganath@users.noreply.github.com>
2018-09-03 21:49:55 +0530

    Add files via upload

---
+++ sketch_aug14a/sketch_aug14a.ino
@@ -0,0 +1,20 @@
+int LED =A1;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED,OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  analogWrite(LED,HIGH);
+  Serial.println("LED HIGH");
+  delay(100);
+  analogWrite(LED,LOW);
+  Serial.println("LED LOW");
+  delay(100);
+  
+
+}
