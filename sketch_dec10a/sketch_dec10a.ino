1e963ac06f52dc0556b75a90d23ed407f0ec65b2
sandun prasanganath <33305140+prasanganath@users.noreply.github.com>
2018-09-03 21:51:32 +0530

    Add files via upload

---
+++ sketch_dec10a/sketch_dec10a.ino
@@ -0,0 +1,20 @@
+void setup() {
+  pinMode(4,INPUT);
+  pinMode(11,OUTPUT);//RED
+  pinMode(12,OUTPUT);//GREEN
+  
+
+}
+
+void loop() {
+ if (digitalRead(4)==LOW){
+  digitalWrite(11,HIGH);
+  digitalWrite(12,LOW);
+
+  
+ }
+ else{
+  digitalWrite(12,HIGH);
+  digitalWrite(11,LOW);
+}
+}
