e1e40f26a4673d2e3b9413ba6061a43f9c51fa15
sandun prasanganath <33305140+prasanganath@users.noreply.github.com>
2018-09-03 21:53:06 +0530

    Add files via upload

---
+++ sketch_sep09b/sketch_sep09b.ino
@@ -0,0 +1,21 @@
+  char val;
+void setup() {
+ pinMode(13,OUTPUT);
+ Serial.begin(9600);
+
+}
+
+void loop() {
+  if(Serial.available()){
+    val=Serial.read();
+    Serial.println(val);
+  }
+  if(val=='1'){
+    digitalWrite(13,HIGH);
+  }
+  else if(val=='2'){
+    digitalWrite(13,LOW);  
+  }
+  delay(100); 
+
+}
