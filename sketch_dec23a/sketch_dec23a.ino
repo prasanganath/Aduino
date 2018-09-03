0f02b9e6bfdae62ca8a400a64729c2b8a3f0711e
sandun prasanganath <33305140+prasanganath@users.noreply.github.com>
2018-09-03 21:51:47 +0530

    Add files via upload

---
+++ sketch_dec23a/sketch_dec23a.ino
@@ -0,0 +1,25 @@
+
+#include<Servo.h>
+Servo sandun;
+void setup() {
+  sandun.attach(9);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int i=1;i<=360;i++){
+    sandun.write(i);
+    delay(5);
+  }
+
+
+
+
+
+  
+ 
+  
+  
+
+}
