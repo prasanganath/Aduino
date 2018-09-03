ace48baa9389359b4beb89d5775fc45c5e5d0682
sandun prasanganath <33305140+prasanganath@users.noreply.github.com>
2018-09-03 21:52:16 +0530

    Add files via upload

---
+++ sketch_sep08a/sketch_sep08a.ino
@@ -0,0 +1,24 @@
+#define trig 2
+#define echo 4
+void setup() {
+ pinMode(trig,OUTPUT);
+ pinMode(echo,INPUT);
+ Serial.begin(9600);
+
+}
+
+void loop() {
+digitalWrite(trig,LOW);
+delayMicroseconds(2);
+digitalWrite(trig,HIGH);
+delayMicroseconds(10);
+digitalWrite(trig,LOW);
+long t =pulseIn(echo,HIGH);
+long inches =t/74/2;
+long cm =t/29/2;
+Serial.print(inches);// 6in 15cm
+Serial.print(" in \t");
+Serial.print(cm);
+Serial.println("cm");
+delay(1000);
+}
