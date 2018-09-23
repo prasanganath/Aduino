413d1de912c5d167b2c95e90678fd686a812bf17
sandun prasanganath <33305140+prasanganath@users.noreply.github.com>
2018-09-23 12:23:56 +0530

    Update sketch_aug14a.ino

--- sketch_aug14a/sketch_aug14a.ino
+++ sketch_aug14a/sketch_aug14a.ino
@@ -1,14 +1,14 @@
 int LED =A1;
 
 void setup() {
-  // put your setup code here, to run once:
+
   pinMode(LED,OUTPUT);
   Serial.begin(9600);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
   analogWrite(LED,HIGH);
   Serial.println("LED HIGH");
   delay(100);
